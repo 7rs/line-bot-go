3f5fb2352c952eeb2f6763b023ce181a56cf864a
7rs <rsytav@gmail.com>
2020-04-26 00:07:49 +0900

    初期設定

---
+++ go.mod
@@ -0,0 +1,15 @@
+module github.com/7rs/line-bot-go
+
+go 1.14
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.6 // indirect
+	github.com/valyala/fasttemplate v1.1.0 // indirect
+	golang.org/x/crypto v0.0.0-20200423211502-4bdfaf469ed5 // indirect
+	golang.org/x/net v0.0.0-20200421231249-e086a090c8fd // indirect
+	golang.org/x/sys v0.0.0-20200420163511-1957bb5e6d1f // indirect
+	golang.org/x/text v0.3.2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "8000"
+	}
+	return port
+}
+
+func mainPage() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, world!")
+	}
+}
+
+func main() {
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/", mainPage())
+
+	err := e.Start(":" + getPort())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
